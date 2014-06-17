b4d68135970151d4cac17c3eb2b21d0de9b9204e
Ivan Boldyrev <lispnik@gmail.com>
2014-06-17 17:23:03 +0700

    Редактирование точек.

--- astromap/views.py
+++ astromap/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from django.contrib.gis.geos import Point
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.utils.translation import get_language_from_request
 from django.views.decorators.http import require_POST, require_safe
 import json
@@ -12,18 +15,21 @@ from astromap import models, utils
 def index(request):
     u""" Index page.
     """
-    kook = utils.get_kook(request)
+    response = HttpResponse(content_type='text/html; charset=utf-8')
+    kook = utils.get_kook(request, response)
     pts = [utils.jsonize(pt, kook) for pt in models.Point.objects.values()]
     lang = get_language_from_request(request)
     map_type = request.GET.get('type', 'normal')
     # Renders template
-    return render(request, 'index.html', {
+
+    response.write(render_to_string('index.html', {
         'PTS_JSON': json.dumps(pts),
         'LANG': lang,
         # TODO use django.utils.translation
         'lang_file_js': 'lang/lang_'+lang+'.js',
         'type': map_type,
-    })
+    }, RequestContext(request)))
+    return response
 
 
 @require_safe
@@ -46,7 +52,96 @@ def login(request, token=None):
 @require_POST
 def ajax_handler(request):
     u""" AJAX handling: point creation, modification, etc. """
-    return HttpResponse(u'ajax')
+    # Мы заранее создаём response, чтобы get_kook установил в нём новую куку.
+    # Затем мы в этот response пишем с помощью json.dump.
+    response = HttpResponse(content_type='application/json')
+    kook = utils.get_kook(request, response)
+    cmd = request.POST.get('cmd', None)
+    ip = utils.inet_aton(request.META['REMOTE_ADDR'])
+
+    if cmd == 'insert':
+        # TODO: insert form
+        title = request.POST['txt'].strip()
+        lat = float(request.POST['lat'])
+        lon = float(request.POST['lon'])
+        zoom = int(request.POST['zoom'])
+        if title:
+            pt = models.Point.objects.create(
+                title=title,
+                mapid=2,
+                zoom=zoom,
+                point=Point(lat, lon),
+                kook=kook,
+                ip=ip,
+                ptxt='',  # Was used for debug of PHP version...
+            )
+            json.dump({
+                'status': 'OK',
+                'id': pt.id
+            }, response)
+        else:
+            json.dump({
+                'status': 'errror',
+                'text': u"Title cannot be empty."
+            }, response)
+
+    elif cmd == 'updattr':
+        # TODO updattr form
+        title = request.POST['txt'].strip()
+        id = int(request.POST['id'])
+        if title:
+            try:
+                pt = models.Point.objects.get(id=id, kook=kook)
+                pt.title = title
+                pt.save()
+            except models.Point.DoesNotExist:
+                pass  # Если точка не найдена, делаем вид, что всё ОК
+            json.dump({
+                'status': 'OK'
+            }, response)
+        else:
+            json.dump({
+                'status': 'errror',
+                'text': u"Title cannot be empty."
+            }, response)
+
+    elif cmd == 'updgeom':
+        # TODO updgeom form
+        id = int(request.POST['id'])
+        lat = float(request.POST['lat'])
+        lon = float(request.POST['lon'])
+        zoom = int(request.POST['zoom'])
+
+        try:
+            pt = models.Point.objects.get(id=id, kook=kook)
+            pt.zoom = zoom
+            pt.point = Point(lat, lon)
+            pt.save()
+        except models.Point.DoesNotExist:
+            pass  # Если точка не найдена или чужая, делаем вид, что всё ОК
+        json.dump({
+            'status': 'OK'
+        }, response)
+
+    elif cmd == 'del':
+        # TODO del form
+        id = int(request.POST['id'])
+
+        try:
+            pt = models.Point.objects.get(id=id, kook=kook)
+            pt.delete()
+        except models.Point.DoesNotExist:
+            pass  # Если точка не найдена или чужая, делаем вид, что всё ОК
+        json.dump({
+            'status': 'OK'
+        }, response)
+    else:
+        json.dump({
+            'status': 'error',
+            'text': u"Unknown command."
+        }, response)
+
+    return response
 
 
 # def auth_handler(request):
