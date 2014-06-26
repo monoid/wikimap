monoid/wikimap

commit f9c84fbbf0210f7d15f8fc7e3a932fc2215db40e

Return 404 if atom's mode is invalid.
It should prevent cache filling with lot of modes.

astromap/views.py
@@ -2,6 +2,7 @@
 from django.contrib.gis.feeds import GeoAtom1Feed, Feed
 from django.contrib.gis.geos import Point
 from django.contrib.gis.geos.polygon import Polygon
+from django.http import Http404
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.template.loader import render_to_string
@@ -56,7 +57,7 @@ class AMGeoAtom1Feed(Feed):
         mode = args.get('mode')
 
         if mode not in self.VALID_MODES:
-            mode = 'latest'
+            raise Http404(u"Incorrect mode.")
 
         if mode in self.WINDOW_MODES:
             if 'lb' in args and 'rt' in args and \

commit b9cfe0a4568c0def8321ff0a73fd12b2409bbea0

Atom's default mode is simple.
Bug introduced by f9c84fbbf0210f7d15f8fc7e3a932fc2215db40e

astromap/views.py
@@ -59,7 +59,7 @@ class AMGeoAtom1Feed(Feed):
 
     def get_object(self, request, *args, **kwargs):
         args = request.GET
-        mode = args.get('mode')
+        mode = args.get('mode', 'simple')
 
         if mode not in self.VALID_MODES:
             raise Http404(u"Incorrect mode.")